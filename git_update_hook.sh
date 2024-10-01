40e9a4650980baa9513516fb1928954fcdca02ec
Cripochec <124876135+Cripochec@users.noreply.github.com>
2024-10-01 17:17:28 +0500

    test 10 auto update cod in server

--- git_update_hook.sh
+++ git_update_hook.sh
@@ -1,5 +1,5 @@
-#!/bin/bashcd /root/FlopyBac
-cd /root/FlopyBac
-git pull origin main
-pip install -r requirements.txt
-sudo supervisorctl restart FlopyBac
+#!/bin/bash
+cd /root/FlopyBac  # Перейти в директорию проекта
+git pull origin main  # Обновить код из репозитория
+pip install -r requirements.txt  # Установить/обновить зависимости
+sudo supervisorctl restart FlopyBac  # Перезапустить приложение через Supervisor
